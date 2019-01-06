35899d91e5a6dd1f2aba7742274938b4afbe1181
ToolMu <toolmumu@gmail.com>
2019-01-06 11:16:33 +0800

    leetcode 09“

---
+++ leetcode/palindrome_number.py
@@ -0,0 +1,38 @@
+class Solution1(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+
+        str_x = str(x)
+        rstr_x = str_x[::-1]
+        if str_x == rstr_x:
+            return True
+
+        return False
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x % 10 == 0 and x != 0):  # 条件2为了避免10这个例子
+            return False
+
+        reverse_num = 0
+        while x > reverse_num:
+            reverse_num = reverse_num * 10 + x % 10
+            x = x // 10  # Py3除法的坑
+            print(x, reverse_num)
+
+        return x == reverse_num or x == reverse_num // 10
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.isPalindrome(11))
