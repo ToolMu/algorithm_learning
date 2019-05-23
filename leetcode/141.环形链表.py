1c8e2529facc505f43bcb727e6e367a2e76355f8
Tool_Mu <Tool_MU@163.com>
2019-05-23 22:38:38 +0800

    [add] leetcode 141

---
+++ leetcode/141.环形链表.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=141 lang=python
+#
+# [141] 环形链表
+#
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                return True
+        return False
+
