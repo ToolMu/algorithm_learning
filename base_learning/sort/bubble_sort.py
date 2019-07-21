436d291b74d54cf29989752048653b7b5960dadd
Tool_Mu <Tool_MU@163.com>
2019-07-21 14:51:42 +0800

    [update] bubble_sort

--- base_learning/sort/bubble_sort.py
+++ base_learning/sort/bubble_sort.py
@@ -8,6 +8,23 @@ def bubble_sort(sorting_list):
     return sorting_list
 
 
+def improved_bubble_sort(_list):
+    count  = len(_list)
+    for i in range(count):  # 找一个位置
+        stop = True
+        for j in range(i+1, count):  # 从当前位置冒出
+            if _list[i] > _list[j]:
+                # 当出现变换时即认为当前排序没完成
+                # 当没有出现变换时即可认为该部分排序已经基本完成
+                stop = False
+                sorting_list[i], sorting_list[j] = _list[j], _list[i]
+        
+        if stop:
+            break
+    
+    return _list
+
+
 if __name__ == "__main__":
     test_data = [3,4,9,7,1]
     print(bubble_sort(test_data))
